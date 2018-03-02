e36f284008b2299f46c545d9a5abe3dfaf39cca4
dannyjones <soeazy123444@gmail.com>
2018-03-02 13:28:07 +0800

    组织架构管理

--- server/public/src/components/business/OrganizationManager.vue
+++ server/public/src/components/business/OrganizationManager.vue
@@ -6,12 +6,11 @@
                     <el-card class="box-card">
                         <div slot="header" class="clearfix">
                             <span>组织架构树</span>
-                            <!-- <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button> -->
                         </div>
                         <el-input placeholder="输入关键字进行过滤" v-model="filterText">
                         </el-input>
-                        <el-tree :filter-node-method="filterNode" :data="depttree" default-expand-all node-key="id" ref="tree" highlight-current
-                            :props="defaultProps" @node-click="handleNodeClick">
+                        <el-tree :expand-on-click-node="false" :filter-node-method="filterNode" :data="depttree" default-expand-all node-key="id"
+                            ref="tree" highlight-current @node-click="handleNodeClick">
                         </el-tree>
                     </el-card>
                 </el-col>
@@ -19,35 +18,59 @@
                     <el-card class="box-card">
                         <div slot="header" class="clearfix">
                             <span>组织架构管理</span>
-                            <!-- <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button> -->
                         </div>
-                        <el-form ref="form" :model="form" label-width="80px">
-                            <el-form-item label="活动名称">
-                                <el-input></el-input>
+                        <el-form ref="form" label-width="80px">
+                            <el-form-item label="父节点">
+                                <el-input :disabled="true" v-model='currentNode.label'></el-input>
                             </el-form-item>
-                            <el-form-item label="活动区域">
-                                <el-select placeholder="请选择活动区域">
-                                    <el-option label="区域一" value="shanghai"></el-option>
-                                    <el-option label="区域二" value="beijing"></el-option>
-                                </el-select>
+                            <el-form-item label="部门ID">
+                                <el-input :disabled="true" v-model='newDeptNode.id'></el-input>
                             </el-form-item>
-                            <el-form-item label="活动时间">
-                                <el-col :span="11">
-                                    <el-date-picker type="date" placeholder="选择日期" style="width: 100%;"></el-date-picker>
-                                </el-col>
-                                <el-col class="line" :span="2">-</el-col>
-                                <el-col :span="11">
-                                    <el-time-picker type="fixed-time" placeholder="选择时间" style="width: 100%;"></el-time-picker>
-                                </el-col>
+                            <el-form-item label="部门名称">
+                                <el-input v-model='newDeptNode.label'></el-input>
                             </el-form-item>
+
                         </el-form>
+                        <div>
+                            <el-button type="primary" @click="addNewDept" :loading="editLoading">新增部门</el-button>
+                            <el-button @click="cancelAddDept">取 消</el-button>
+                        </div>
                     </el-card>
 
                 </el-col>
             </el-row>
         </el-tab-pane>
         <el-tab-pane label="职位管理">
+            <el-card class="box-card">
+                <div slot="header" class="clearfix">
+                    <span>职位表</span>
+                    <el-button style="float: right; padding: 3px 0" @click="newTag" type="text">新增职位</el-button>
+                </div>
+                <el-tag v-for="tag in PostTags" :key="tag.name" closable :type="tag.type" @close="delTag(tag)">
+                    {{tag.name}}
+                </el-tag>
+            </el-card>
 
+            <el-card class="box-card">
+                <div slot="header" class="clearfix">
+                    <span>职位管理</span>
+                </div>
+                <el-form ref="form" label-width="80px">
+                    <el-form-item label="职位ID">
+                        <el-input :disabled="true" v-model='newPost.postid'></el-input>
+                    </el-form-item>
+                    <el-form-item label="职位名称">
+                        <el-input v-model='newPost.postname'></el-input>
+                    </el-form-item>
+                    <el-form-item label="部门类型">
+                        <el-input v-model='newPost.posttype'></el-input>
+                    </el-form-item>
+                </el-form>
+                <div>
+                    <el-button type="primary" @click="addNewDept" :loading="editLoading">新增部门</el-button>
+                    <el-button @click="cancelAddDept">取 消</el-button>
+                </div>
+            </el-card>
         </el-tab-pane>
     </el-tabs>
 </template>
@@ -61,30 +84,59 @@
         data() {
             return {
                 filterText: '',
-                depttree: [
-                    {
-                        id: 1, label: 'Root', children: [{
-                            id: 2, label: '总经办'
-                        },
-                        {
-                            id: 3, label: '市场部', children: [
-                                { id: 5, label: '军工组' },
-                                { id: 6, label: '气象组' }
-                            ]
-                        },
-                        { id: 4, label: '研发部' }]
-                    },
-                ]
+                depttree: [{ id: "1", label: 'Root', children: [] }],
+                currentNode: {},
+                newDeptNode: {
+                    label: '',
+                    id: 0,
+                    children: []
+                },
+                editLoading: false,
+                listLoading: false,
+                PostTags: [
+                    { name: '精算师', type: 'success', index: '1-1' },
+                    { name: '部门经理', type: 'info', index: '1-2' },
+                    { name: '项目经理', type: 'warning', index: '1-3' },
+                    { name: '预算师', type: 'danger', index: '1-4' },
+                ],
+                newPost: {
+                    postid: '',
+                    postname: '',
+                    posttype: ''
+                }
             }
         },
+        created() {
+            this.newPost.postid = this.PostTags.length + 1
+        },
         methods: {
+            newTag() { },
+            delTag() { },
+            computeNewID() {
+                if (this.currentNode.children) {
+                    this.newDeptNode.id = this.currentNode.id + '-' + (this.currentNode.children.length + 1)
+                } else {
+                    this.newDeptNode.id = this.currentNode.id + '-1'
+                }
+            },
+            addNewDept() {
+                var item = { id: this.newDeptNode.id, label: this.newDeptNode.label, children: [] }
+                if (!this.currentNode.children) {
+                    this.$set(this.currentNode, 'children', []);
+                }
+                this.currentNode.children.push(item);
+                this.newDeptNode = { id: '', label: '', children: [] }
+                this.computeNewID()
+            },
+            cancelAddDept() { },
             filterNode(value, data) {
                 if (!value) return true;
                 return data.label.indexOf(value) !== -1;
             },
             handleNodeClick(data) {
-                console.log(data)
-            }
+                this.currentNode = data
+                this.computeNewID()
+            },
         }
     }
 </script>
