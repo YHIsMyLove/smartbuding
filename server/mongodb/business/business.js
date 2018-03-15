9d57845161c6af74dea9d47b98817b2da4a85beb
dannyjones <soeazy123444@gmail.com>
2018-03-15 22:23:22 +0800

    插入/删除用户部门表

--- server/mongodb/business/business.js
+++ server/mongodb/business/business.js
@@ -89,19 +89,47 @@ exports.GetUsersByUserDept_ProjID = async (req, res) => {
     }
 
     //1. 查询总数
-    let count = await UserDept.find({ ProjID: query.ProjID }).count()
+    //let count = await UserDept.find({ ProjID: query.ProjID }).count()
     //2. 查询IDs
-    let users = await UserDept.find(query).select("UserID")
-        .populate({
-            path: 'User',
-            select: "UserName UserSex UserAge UserPhoneNum"
-        })
+    //let users = await UserDept.find(query).select("UserID")
+    //    .populate({
+    //        path: 'User',
+    //        select: "UserName UserSex UserAge UserPhoneNum"
+    //    })
     //3. 查询人员信息
-    let users = await user.find({ UserID: { $in: ids } })
+    //let users = await user.find({ UserID: { $in: ids } })
 }
 
 //设置用户部门表
 exports.SetUser2DeptByUserID = async (req, res) => {
-    let query = {}
-
+    let insertOrDel = req.body.insertOrDel
+    if (!insertOrDel) {
+        return res.send(msg.genFailedMsg('请输入插入/删除'))
+    }
+    let query = {
+        UserID: req.body.UserID,
+        ProjID: req.body.ProjID,
+        DeptID: req.body.DeptID,
+    }
+    if (!query.UserID) {
+        return res.send(msg.genFailedMsg('请输入人员ID'))
+    }
+    if (!query.ProjID) {
+        return res.send(msg.genFailedMsg('请输入项目ID'))
+    }
+    if (!query.DeptID) {
+        return res.send(msg.genFailedMsg('请输入部门ID'))
+    }
+    try {
+        let _userDept = new UserDept(query)
+        if (insertOrDel == 'insert') {
+            await _userDept.updateAndSave()
+            res.send(msg.genMsg('保存成功'))
+        } else {
+            await UserDept.findOne(query).remove()
+            res.send(msg.genMsg('删除成功'))
+        }
+    } catch{
+        res.send(msg.getFailedMsg('保存失败'))
+    }
 }
