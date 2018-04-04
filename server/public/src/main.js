74dfe21bdff1dd62c3549879f264adfaa20083b2
dannyjones <soeazy123444@gmail.com>
2018-04-04 17:39:31 +0800

    路由懒加载!

--- server/public/src/main.js
+++ server/public/src/main.js
@@ -11,19 +11,19 @@ import 'nprogress/nprogress.css'
 import Login from './components/Login.vue'
 import Home from './components/Home.vue'
 import Main from './components/Main.vue'
-import UserManager from './components/business/UserManager.vue'
-import UserDeptManager from './components/business/UserDeptManager.vue'
-import UserProjManager from './components/business/UserProjManager.vue'
-import UserRoleManager from './components/business/UserRoleManager.vue'
-import SysFieldManager from './components/nav1/SysFieldManager.vue'
-import SystemManager from './components/business/SystemManager.vue'
-import DeviceRoleManager from './components/business/DeviceRoleManager.vue'
-import DeviceManager from './components/business/DeviceManager.vue'
-import YSDeviceManager from './components/business/YSDeviceManager.vue'
-import SystemRoleManager from './components/business/SystemRoleManager.vue'
-import Meeting from './components/business/Meeting.vue'
-import FileManager from './components/business/FileManager.vue'
-import MeetingMinutesManager from './components/business/MeetingMinutesManager.vue'
+// import UserManager from './components/business/UserManager.vue'
+// import UserDeptManager from './components/business/UserDeptManager.vue'
+// import UserProjManager from './components/business/UserProjManager.vue'
+// import UserRoleManager from './components/business/UserRoleManager.vue'
+// import SysFieldManager from './components/nav1/SysFieldManager.vue'
+// import SystemManager from './components/business/SystemManager.vue'
+// import DeviceRoleManager from './components/business/DeviceRoleManager.vue'
+// import DeviceManager from './components/business/DeviceManager.vue'
+// import YSDeviceManager from './components/business/YSDeviceManager.vue'
+// import SystemRoleManager from './components/business/SystemRoleManager.vue'
+// import Meeting from './components/business/Meeting.vue'
+// import FileManager from './components/business/FileManager.vue'
+// import MeetingMinutesManager from './components/business/MeetingMinutesManager.vue'
 
 Vue.use(ElementUI)
 Vue.use(VueRouter)
@@ -42,15 +42,22 @@ const routes = [
         iconCls: 'fa fa-id-card-o',//图标样式class
         children: [
             {
-                path: '/', component: Main, name: '主页',
+                path: '/',
+                component: Main,
+                name: '主页',
                 meta: { redirectAuth: true }
             },
             {
-                path: '/SysFieldManager', component: SysFieldManager, name: '系统管理',
+                path: '/SysFieldManager',
+                component: resolve =>
+                    require(['./components/nav1/SysFieldManager.vue'], resolve),
+                name: '系统管理',
                 meta: { redirectAuth: true }
             },
             {
-                path: '/FileManager', component: FileManager, name: '文件管理',
+                path: '/FileManager',
+                component: resolve => require(['./components/business/FileManager.vue'], resolve),
+                name: '文件管理',
                 meta: { redirectAuth: true }
             },
         ],
@@ -63,19 +70,27 @@ const routes = [
         iconCls: 'fa fa-id-card-o',
         children: [
             {
-                path: '/UserManager', component: UserManager, name: '用户管理',
+                path: '/UserManager',
+                component: resolve => require(['./components/business/UserManager.vue'], resolve),
+                name: '用户管理',
                 meta: { redirectAuth: true }
             },
             {
-                path: '/UserProjManager', component: UserProjManager, name: '用户项目管理',
+                path: '/UserProjManager',
+                component: resolve => require(['./components/business/UserProjManager.vue'], resolve),
+                name: '用户项目管理',
                 meta: { redirectAuth: true }
             },
             {
-                path: '/UserDeptManager', component: UserDeptManager, name: '用户部门管理',
+                path: '/UserDeptManager',
+                component: resolve => require(['./components/business/UserDeptManager.vue'], resolve),
+                name: '用户部门管理',
                 meta: { redirectAuth: true }
             },
             {
-                path: '/UserRoleManager', component: UserRoleManager, name: '用户角色管理',
+                path: '/UserRoleManager',
+                component: resolve => require(['./components/business/UserRoleManager.vue'], resolve),
+                name: '用户角色管理',
                 meta: { redirectAuth: true }
             },
             // { path: '/vuex', component: VuexComp, name: 'Vuex' }
@@ -89,11 +104,15 @@ const routes = [
         iconCls: 'fa fa-id-card-o',
         children: [
             {
-                path: '/DeviceManager', component: DeviceManager, name: '设备管理',
+                path: '/DeviceManager',
+                component: resolve => require(['./components/business/DeviceManager.vue'], resolve),
+                name: '设备管理',
                 meta: { redirectAuth: true }
             },
             {
-                path: '/YSDeviceManager', component: YSDeviceManager, name: '萤石设备管理',
+                path: '/YSDeviceManager',
+                component: resolve => require(['./components/business/YSDeviceManager.vue'], resolve),
+                name: '萤石设备管理',
                 meta: { redirectAuth: true }
             },
         ],
@@ -106,7 +125,9 @@ const routes = [
         iconCls: 'fa fa-id-card-o',
         children: [
             {
-                path: '/Meeting', component: Meeting, name: '会议管理',
+                path: '/Meeting',
+                component: resolve => require(['./components/business/Meeting.vue'], resolve),
+                name: '会议管理',
                 meta: { redirectAuth: true }
             },
         ],
