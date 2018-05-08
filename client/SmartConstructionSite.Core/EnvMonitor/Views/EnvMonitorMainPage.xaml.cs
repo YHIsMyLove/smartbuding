ca41f58a324236831abfb549c20aa6685223e8de
lbgongfu <lbgongfu@163.com>
2018-05-09 00:16:19 +0800

    环境监测页面添加柱状图表

--- client/SmartConstructionSite.Core/EnvMonitor/Views/EnvMonitorMainPage.xaml.cs
+++ client/SmartConstructionSite.Core/EnvMonitor/Views/EnvMonitorMainPage.xaml.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using SmartConstructionSite.Core.Common;
 using Xamarin.Forms;
@@ -7,50 +7,9 @@ namespace SmartConstructionSite.Core.EnvMonitor.Views
 {
     public partial class EnvMonitorMainPage : ContentPage
     {
-        private static readonly string html = @"<!DOCTYPE html>"
-            + "<html>"
-            +   "<head>"
-            +       "<meta charset=\"utf-8\">"
-            +       "<title>ECharts</title>"
-            +       "<script>"
-            +       "</script>"
-            +   "</head>"
-            +   "<body>"
-            +       "<div id = \"main\" style=\"width: 600px;height:400px;\" ></div>"
-            +       "<script type= \"text/javascript\">"
-            +           "var myChart = echarts.init(document.getElementById('main'));"
-
-            +           "var option = {"
-            +               "title: {"
-            +                   "text: 'ECharts 入门示例'"
-            +               "},"
-            +               "tooltip: {},"
-            +               "legend: {"
-            +                   "data:['销量']"
-            +               "},"
-            +               "xAxis: {"
-            +                   "data: [\"衬衫\",\"羊毛衫\",\"雪纺衫\",\"裤子\",\"高跟鞋\",\"袜子\"]"
-            +               "},"
-            +               "yAxis: {},"
-            +               "series: [{"
-            +                   "name: '销量',"
-            +                   "type: 'bar',"
-            +                   "data: [5, 20, 36, 10, 10, 20]"
-            +               "}]"
-            +           "};"
-
-            +           "myChart.setOption(option);"
-            +       "</script>"
-            +   "</body>"
-            + "</html>";
         public EnvMonitorMainPage()
         {
             InitializeComponent();
-<<<<<<< HEAD
-            var htmlWebViewSource = new HtmlWebViewSource();
-            htmlWebViewSource.Html = @"";
-            chartContainer.Source = htmlWebViewSource;
-=======
 
             var source = new HtmlWebViewSource();
             source.Html = LoadHtml();
@@ -61,7 +20,6 @@ namespace SmartConstructionSite.Core.EnvMonitor.Views
         private string LoadHtml()
         {
             return DependencyService.Get<ISaveAndLoad>().LoadAsset("BarChart.html");
->>>>>>> 7925ba3... 环境监测添加柱状图表
         }
     }
 }
