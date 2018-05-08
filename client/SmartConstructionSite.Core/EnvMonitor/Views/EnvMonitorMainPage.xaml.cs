d172be7c827f66625ffddca8e7390bf0cbb9305d
LiBiao <lbgongfu@163.com>
2018-05-08 16:41:30 +0800

    完善环境监测页面

--- client/SmartConstructionSite.Core/EnvMonitor/Views/EnvMonitorMainPage.xaml.cs
+++ client/SmartConstructionSite.Core/EnvMonitor/Views/EnvMonitorMainPage.xaml.cs
@@ -7,9 +7,48 @@ namespace SmartConstructionSite.Core.EnvMonitor.Views
 {
     public partial class EnvMonitorMainPage : ContentPage
     {
+        private static readonly string html = @"<!DOCTYPE html>"
+            + "<html>"
+            +   "<head>"
+            +       "<meta charset=\"utf-8\">"
+            +       "<title>ECharts</title>"
+            +       "<script>"
+            +       "</script>"
+            +   "</head>"
+            +   "<body>"
+            +       "<div id = \"main\" style=\"width: 600px;height:400px;\" ></div>"
+            +       "<script type= \"text/javascript\">"
+            +           "var myChart = echarts.init(document.getElementById('main'));"
+
+            +           "var option = {"
+            +               "title: {"
+            +                   "text: 'ECharts 入门示例'"
+            +               "},"
+            +               "tooltip: {},"
+            +               "legend: {"
+            +                   "data:['销量']"
+            +               "},"
+            +               "xAxis: {"
+            +                   "data: [\"衬衫\",\"羊毛衫\",\"雪纺衫\",\"裤子\",\"高跟鞋\",\"袜子\"]"
+            +               "},"
+            +               "yAxis: {},"
+            +               "series: [{"
+            +                   "name: '销量',"
+            +                   "type: 'bar',"
+            +                   "data: [5, 20, 36, 10, 10, 20]"
+            +               "}]"
+            +           "};"
+
+            +           "myChart.setOption(option);"
+            +       "</script>"
+            +   "</body>"
+            + "</html>";
         public EnvMonitorMainPage()
         {
             InitializeComponent();
+            var htmlWebViewSource = new HtmlWebViewSource();
+            htmlWebViewSource.Html = @"";
+            chartContainer.Source = htmlWebViewSource;
         }
     }
 }
