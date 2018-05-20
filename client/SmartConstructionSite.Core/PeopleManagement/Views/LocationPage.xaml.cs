10daff0e2429fe5ee5533d65ff6d097dde2aa6a8
lbgongfu <lbgongfu@163.com>
2018-05-20 23:57:52 +0800

    添加新框架（Cordova+vue2+Framework7）

---
+++ client/SmartConstructionSite.Core/PeopleManagement/Views/LocationPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SmartConstructionSite.Core.PeopleManagement.Views
+{
+    public partial class LocationPage : ContentPage
+    {
+        public LocationPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
