3397872c9a2cd333cae2eb9094ae8ea16c56baae
dannyjones <soeazy123444@gmail.com>
2018-05-25 16:51:17 +0800

    主页组件化

--- server/public/src/components/layout/Home.vue
+++ server/public/src/components/layout/Home.vue
@@ -3,143 +3,63 @@
     <el-col :span="24">
         <NavBar :iscollapse='iscollapse' @ChangeCollapse='changeMenu' />
     </el-col>
-<el-col :span="24" class="panel-center">
-    <aside class="left-aside">
-        <div class="panel-User">
-        <el-col :span="8">
-        <div class="user-img">
-        <img style="border-radius: 50%;vertical-align: middle;widht:32px;height:32px" src="../assets/user.png" alt="user">
-        </div>
-        </el-col>
-        <el-col :span="8">
-        <div style="margin-top:12px">
-        <span style="font-size:15px">
-        <b>用户1</b></span>
-        <br>
-        <span style="font-size:10px;color:#C0C4CC">经理</span>
-        </div>
-        </el-col>
-        <el-col :span="8">
-        <div style="margin-top:12px">
-        <el-tag type="info"> 工程师</el-tag>
-        </div>
-        </el-col>
-        <el-col :span="24">
-        <hr style="height:.5px;border:none;border-top:1px solid #ffff;" />
-        </el-col>
-        </div>
-        <el-menu :collapse='iscollapse' :default-active="$route.path" :default-openeds="openedArr" collapse-transition class="aside-menu" theme="dark" unique-opened router>
-        <template v-for="(item,index) in $router.options.routes" v-if="!item.hidden">
-        <el-submenu
-        :key="index"
-        :index="index+''"
-        v-if="!item.leaf"
-        >
-        <template slot="title">
-        <i :class="item.iconCls"></i>{{item.name}}
-        </template>
+    <el-col :span="24" class="panel-center">
+        <aside class="left-aside">
+            <div class="panel-User">
+            <el-col :span="8">
+            <div class="user-img">
+            <img style="border-radius: 50%;vertical-align: middle;widht:32px;height:32px" src="../../assets/user.png" alt="user">
+            </div>
+            </el-col>
+            <el-col :span="8">
+            <div style="margin-top:12px">
+            <span style="font-size:15px">
+            <b>用户1</b></span>
+            <br>
+            <span style="font-size:10px;color:#C0C4CC">经理</span>
+            </div>
+            </el-col>
+            <el-col :span="8">
+            <div style="margin-top:12px">
+            <el-tag type="info"> 工程师</el-tag>
+            </div>
+            </el-col>
+            <el-col :span="24">
+            <hr style="height:.5px;border:none;border-top:1px solid #ffff;" />
+            </el-col>
+            </div>
+            <el-menu :collapse='iscollapse' :default-active="$route.path" :default-openeds="openedArr" collapse-transition class="aside-menu" theme="dark" unique-opened router>
+            <template v-for="(item,index) in $router.options.routes" v-if="!item.hidden">
+            <el-submenu
+            :key="index"
+            :index="index+''"
+            v-if="!item.leaf"
+            >
+            <template slot="title">
+            <i :class="item.iconCls"></i>{{item.name}}
+            </template>
 
-        <el-menu-item
-        :key="child.path"
-        v-for="child in item.children"
-        v-if="!child.hidden"
-        :index="child.path"
-        >
-        {{child.name}}
-        </el-menu-item>
-        </el-submenu>
-        <el-menu-item
-        :key="item.children[0].path"
-        v-if="item.leaf&&item.children.length>0"
-        :index="item.children[0].path"
-        >
-        <i :class="item.iconCls"></i>{{item.children[0].name}}</el-menu-item>
-        </template>
+            <el-menu-item
+            :key="child.path"
+            v-for="child in item.children"
+            v-if="!child.hidden"
+            :index="child.path"
+            >
+            {{child.name}}
+            </el-menu-item>
+            </el-submenu>
+            <el-menu-item
+            :key="item.children[0].path"
+            v-if="item.leaf&&item.children.length>0"
+            :index="item.children[0].path"
+            >
+            <i :class="item.iconCls"></i>{{item.children[0].name}}</el-menu-item>
+            </template>
 
-        </el-menu>
-    </aside>
-    <section :class="contentStyle">
-        <div class="grid-content bg-purple-light">
-        <el-col
-        :span="24"
-        class="panel-inner-top"
-        >
-        <strong>{{$route.name}}</strong>
-        <el-breadcrumb
-        separator="/"
-        class="bread-crumb"
-        >
-        <el-breadcrumb-item
-        :to="{ path: '/' }"
-        v-if="$route.path!='/'"
-        >首页</el-breadcrumb-item>
-        <el-breadcrumb-item v-if="$route.path!='/'">{{$route.matched[0].name}}</el-breadcrumb-item>
-        <el-breadcrumb-item>{{$route.name}}</el-breadcrumb-item>
-        </el-breadcrumb>
-        </el-col>
-        <el-col
-        :span="24"
-        class="panel-inner-bottom"
-        >
-        <router-view></router-view>
-        </el-col>
-        </div>
-    </section>
-</el-col>
-</el-row>
-<el-col :span="24" class="panel-center">
-    <aside class="left-aside">
-        <div class="panel-User">
-        <el-col :span="8">
-        <div class="user-img">
-        <img style="border-radius: 50%;vertical-align: middle;widht:32px;height:32px" src="../assets/user.png" alt="user">
-        </div>
-        </el-col>
-        <el-col :span="8">
-        <div style="margin-top:12px">
-        <span style="font-size:15px">
-        <b>用户1</b></span>
-        <br>
-        <span style="font-size:10px;color:#C0C4CC">经理</span>
-        </div>
-        </el-col>
-        <el-col :span="8">
-        <div style="margin-top:12px">
-        <el-tag type="info"> 工程师</el-tag>
-        </div>
-        </el-col>
-        <el-col :span="24">
-        <hr style="height:.5px;border:none;border-top:1px solid #ffff;" />
-        </el-col>
-        </div>
-        <el-menu :collapse='iscollapse' :default-active="$route.path" :default-openeds="openedArr" collapse-transition class="aside-menu"  theme="dark" unique-opened router>
-        <template v-for="(item,index) in $router.options.routes" v-if="!item.hidden">
-        <el-submenu :key="index" :index="index+''" v-if="!item.leaf">
-        <template slot="title">
-        <i :class="item.iconCls"></i>{{item.name}}
-        </template>
-
-        <el-menu-item
-        :key="child.path"
-        v-for="child in item.children"
-        v-if="!child.hidden"
-        :index="child.path"
-        >
-        {{child.name}}
-        </el-menu-item>
-        </el-submenu>
-        <el-menu-item
-        :key="item.children[0].path"
-        v-if="item.leaf&&item.children.length>0"
-        :index="item.children[0].path"
-        >
-        <i :class="item.iconCls"></i>{{item.children[0].name}}</el-menu-item>
-        </template>
-
-        </el-menu>
-    </aside>
-    <section :class="contentStyle">
-        <div class="grid-content bg-purple-light">
+            </el-menu>
+        </aside>
+        <section :class="contentStyle">
+            <div class="grid-content bg-purple-light">
             <el-col
             :span="24"
             class="panel-inner-top"
@@ -150,10 +70,13 @@
             class="bread-crumb"
             >
             <el-breadcrumb-item
-            :to="{ path: '/' }"
-            v-if="$route.path!='/'"
-            >首页</el-breadcrumb-item>
-            <el-breadcrumb-item v-if="$route.path!='/'">{{$route.matched[0].name}}</el-breadcrumb-item>
+              :to="{ path: '/' }"
+              v-if="$route.path!='/'"
+              >首页
+            </el-breadcrumb-item>
+            <el-breadcrumb-item v-if="$route.path!='/'">
+              {{$route.matched[0].name}}
+            </el-breadcrumb-item>
             <el-breadcrumb-item>{{$route.name}}</el-breadcrumb-item>
             </el-breadcrumb>
             </el-col>
@@ -163,14 +86,93 @@
             >
             <router-view></router-view>
             </el-col>
-        </div>
-    </section>
-</el-col>
+            </div>
+        </section>
+    </el-col>
+    <el-col :span="24" class="panel-center">
+        <aside class="left-aside">
+            <div class="panel-User">
+            <el-col :span="8">
+            <div class="user-img">
+            <img style="border-radius: 50%;vertical-align: middle;widht:32px;height:32px" src="../../assets/user.png" alt="user">
+            </div>
+            </el-col>
+            <el-col :span="8">
+            <div style="margin-top:12px">
+            <span style="font-size:15px">
+            <b>用户1</b></span>
+            <br>
+            <span style="font-size:10px;color:#C0C4CC">经理</span>
+            </div>
+            </el-col>
+            <el-col :span="8">
+            <div style="margin-top:12px">
+            <el-tag type="info"> 工程师</el-tag>
+            </div>
+            </el-col>
+            <el-col :span="24">
+            <hr style="height:.5px;border:none;border-top:1px solid #ffff;" />
+            </el-col>
+            </div>
+            <el-menu :collapse='iscollapse' :default-active="$route.path" :default-openeds="openedArr" collapse-transition class="aside-menu"  theme="dark" unique-opened router>
+            <template v-for="(item,index) in $router.options.routes" v-if="!item.hidden">
+            <el-submenu :key="index" :index="index+''" v-if="!item.leaf">
+              <template slot="title">
+              <i :class="item.iconCls"></i>{{item.name}}
+              </template>
+
+              <el-menu-item
+              :key="child.path"
+              v-for="child in item.children"
+              v-if="!child.hidden"
+              :index="child.path"
+              >
+              {{child.name}}
+              </el-menu-item>
+            </el-submenu>
+            <el-menu-item
+              :key="item.children[0].path"
+              v-if="item.leaf&&item.children.length>0"
+              :index="item.children[0].path"
+            >
+            <i :class="item.iconCls"></i>{{item.children[0].name}}</el-menu-item>
+            </template>
+
+            </el-menu>
+        </aside>
+        <section :class="contentStyle">
+            <div class="grid-content bg-purple-light">
+                <el-col
+                :span="24"
+                class="panel-inner-top"
+                >
+                <strong>{{$route.name}}</strong>
+                <el-breadcrumb
+                separator="/"
+                class="bread-crumb"
+                >
+                <el-breadcrumb-item
+                :to="{ path: '/' }"
+                v-if="$route.path!='/'"
+                >首页</el-breadcrumb-item>
+                <el-breadcrumb-item v-if="$route.path!='/'">{{$route.matched[0].name}}</el-breadcrumb-item>
+                <el-breadcrumb-item>{{$route.name}}</el-breadcrumb-item>
+                </el-breadcrumb>
+                </el-col>
+                <el-col
+                :span="24"
+                class="panel-inner-bottom"
+                >
+                <router-view></router-view>
+                </el-col>
+            </div>
+        </section>
+    </el-col>
 </el-row>
 </template>
 
 <script>
-import NavBar from "../businesscontrols/NavBar";
+import NavBar from "../businesscontrols/NavBar.vue";
 export default {
   components: {
     NavBar
