2272e16c0db5d1d7da7eacfd2709416c16394736
dannyjones <soeazy123444@gmail.com>
2018-05-27 21:00:05 +0800

    echarts

--- server/public/src/components/layout/components/HomeRankingList.vue
+++ server/public/src/components/layout/components/HomeRankingList.vue
@@ -8,10 +8,21 @@
 </template>
 
 <script>
-let echarts = require("echarts");
-require("echarts/lib/chart/bar");
-require("echarts/lib/component/tooltip");
-require("echarts/lib/component/title");
+import "echarts/lib/chart/bar";
+import "echarts/lib/chart/line";
+import "echarts/lib/chart/pie";
+import "echarts/lib/chart/map";
+import "echarts/lib/chart/radar";
+import "echarts/lib/chart/scatter";
+import "echarts/lib/chart/effectScatter";
+import "echarts/lib/component/tooltip";
+import "echarts/lib/component/polar";
+import "echarts/lib/component/geo";
+import "echarts/lib/component/legend";
+import "echarts/lib/component/title";
+import "echarts/lib/component/visualMap";
+import "echarts/lib/component/dataset";
+import "echarts-liquidfill";
 // import ECharts from "vue-echarts/components/ECharts";
 // import _echarts from "echarts";
 export default {
