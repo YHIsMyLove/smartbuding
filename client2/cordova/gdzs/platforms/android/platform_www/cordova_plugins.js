44c5d7c3ee90c1b9d54038dcb2bafadb67478950
LiBiao <lbgongfu@163.com>
2018-05-30 16:24:49 +0800

    更新ignore

--- client2/cordova/gdzs/platforms/android/platform_www/cordova_plugins.js
+++ client2/cordova/gdzs/platforms/android/platform_www/cordova_plugins.js
@@ -1,11 +1,11 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
-    "id": "cordova-plugin-splashscreen.SplashScreen",
-    "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
-    "pluginId": "cordova-plugin-splashscreen",
+    "id": "call-number.CallNumber",
+    "file": "plugins/call-number/www/CallNumber.js",
+    "pluginId": "call-number",
     "clobbers": [
-      "navigator.splashscreen"
+      "call"
     ]
   },
   {
@@ -39,16 +39,25 @@ module.exports = [
     "clobbers": [
       "scanner"
     ]
+  },
+  {
+    "id": "cordova-plugin-splashscreen.SplashScreen",
+    "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+    "pluginId": "cordova-plugin-splashscreen",
+    "clobbers": [
+      "navigator.splashscreen"
+    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-splashscreen": "5.0.2",
-  "cordova-plugin-whitelist": "1.3.3",
+  "call-number": "1.0.4",
   "cordova-plugin-dialogs": "2.0.1",
   "cordova-plugin-device": "2.0.2",
-  "com.manateeworks.barcodescanner": "1.8.1"
+  "com.manateeworks.barcodescanner": "1.8.1",
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-splashscreen": "5.0.2"
 };
 // BOTTOM OF METADATA
 });
--- client2/cordova/gdzs/platforms/browser/platform_www/cordova_plugins.js
+++ client2/cordova/gdzs/platforms/browser/platform_www/cordova_plugins.js
@@ -1,9 +1,75 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/call-number/www/CallNumber.js",
+        "id": "call-number.CallNumber",
+        "pluginId": "call-number",
+        "clobbers": [
+            "call"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/browser/notification.js",
+        "id": "cordova-plugin-dialogs.notification_browser",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/src/browser/DeviceProxy.js",
+        "id": "cordova-plugin-device.DeviceProxy",
+        "pluginId": "cordova-plugin-device",
+        "runs": true
+    },
+    {
+        "file": "plugins/com.manateeworks.barcodescanner/www/MWBScanner.js",
+        "id": "com.manateeworks.barcodescanner.Scanner",
+        "pluginId": "com.manateeworks.barcodescanner",
+        "clobbers": [
+            "scanner"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/src/browser/SplashScreenProxy.js",
+        "id": "cordova-plugin-splashscreen.SplashScreenProxy",
+        "pluginId": "cordova-plugin-splashscreen",
+        "runs": true
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.3"
+    "call-number": "1.0.4",
+    "cordova-plugin-dialogs": "2.0.1",
+    "cordova-plugin-device": "2.0.2",
+    "com.manateeworks.barcodescanner": "1.8.1",
+    "cordova-plugin-whitelist": "1.3.3",
+    "cordova-plugin-splashscreen": "5.0.2"
 }
 // BOTTOM OF METADATA
 });
