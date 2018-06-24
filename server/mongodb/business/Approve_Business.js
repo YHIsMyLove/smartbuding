31f54c2bece204cc1def41db2d613e55b75c3fcf
Danny Jones <soeazy123444@gmail.com>
2018-06-25 01:40:06 +0800

    审批

--- server/mongodb/business/Approve_Business.js
+++ server/mongodb/business/Approve_Business.js
@@ -3,6 +3,7 @@ const msg = require("../../utils/message")
 const SystemConfig = require('../../config/config')
 const Approve = mongoose.model('Approve')
 const ApproveLog = mongoose.model('ApproveLog')
+const moment = require('moment')
 
 var createApprove = async (req, res) => {
     try {
@@ -24,13 +25,12 @@ var createApprove = async (req, res) => {
 }
 
 var listApprove = async (req, res) => {
-    let query = {}
     try {
-        Object.keys(req.query).forEach(item => {
-            query[item] = req.query[item]
-        })
+        // Object.keys(req.query).forEach(item => {
+        //     query[item] = req.query[item]
+        // })
         let result = await ApproveLog.aggregate([
-            { $match: query },
+            { $match: { ProjID: req.query.ProjID } },
             { $lookup: { from: 'approves', localField: '_approveID', foreignField: '_id', as: 'approves' } },
             { $lookup: { from: 'users', localField: 'proposer.id', foreignField: 'UserID', as: 'proposerInfo' } },
             { $lookup: { from: 'users', localField: 'approver.id', foreignField: 'UserID', as: 'approverInfo' } },
@@ -43,27 +43,26 @@ var listApprove = async (req, res) => {
                     UserName: item.proposerInfo[0].UserName,
                     status: item.proposer.status,
                     content: item.proposer.content,
-                    time: item.proposer.time
+                    time: moment(item.proposer.time).format('YYYY-MM-DD hh:mm')
                 },
                 approver: {
                     _id: item.approverInfo[0]._id,
                     UserName: item.approverInfo[0].UserName,
                     status: item.approver.status,
                     content: item.approver.content,
-                    time: item.approver.time
+                    time: moment(item.approver.time).format('YYYY-MM-DD hh:mm')
                 },
                 overseer: {
                     _id: item.overseerInfo[0]._id,
                     UserName: item.overseerInfo[0].UserName,
                     status: item.overseer.status,
                     content: item.overseer.content,
-                    time: item.overseer.time
+                    time: moment(item.overseer.time).format('YYYY-MM-DD hh:mm')
                 },
                 approveContent: item.approves[0].approveContent,
                 startTime: item.approves[0].startTime,
             }
         })
-
         res.send(msg.genSuccessMsg('查询成功', result))
     } catch (error) {
         res.send(msg.genFailedMsg('查询失败->' + error))
@@ -78,29 +77,35 @@ var listApprove = async (req, res) => {
  *  1 完成
  */
 var optionApprove = async (req, res) => {
-    var UserKind = req.body.UserKind
-    var ApproveID = req.body.ApproveID
-    var query = UserKind === 'proposer'
-        ? {
-            _approveID: ApproveID,
-            'proposer.id': req.body.UserID
-        }
-        : UserKind == 'approver'
+    try {
+        var UserKind = req.body.UserKind
+        var ApproveID = req.body.ApproveID
+        var query = UserKind === 'proposer'
             ? {
                 _approveID: ApproveID,
-                'approver.id': req.body.UserID
+                'proposer.id': req.body.UserID
             }
-            : {
-                _approveID: ApproveID,
-                'overseer.id': req.body.UserID
-            }
-    await ApproveLog.updateOne(query,
-        {
-            'proposer.status': req.body.status,
-            'proposer.content': req.body.content,
-            'proposer.time': Date.now()
-        })
-    res.send(msg.genSuccessMsg('操作完成'))
+            : UserKind == 'approver'
+                ? {
+                    _approveID: ApproveID,
+                    'approver.id': req.body.UserID
+                }
+                : {
+                    _approveID: ApproveID,
+                    'overseer.id': req.body.UserID
+                }
+        await ApproveLog.updateOne(query,
+            {
+                'proposer.status': req.body.status,
+                'proposer.content': req.body.content,
+                'proposer.time': Date.now()
+            })
+        let result = await ApproveLog.findOne(query)
+        res.send(msg.genSuccessMsg('操作完成', result))
+    } catch (error) {
+        res.send(msg.genFailedMsg('error->' + error))
+    }
+
 }
 
 exports.ListApprove = listApprove
